a2a73ef4398c75e41e8216ad31c9d2c576211766
Lorenzo Maffucci <lorenzo.maffucci@edu.unifi.it>
2025-12-15 22:46:56 +0100

    Formattazione dei workflow

--- setup.sh
+++ setup.sh
@@ -8,7 +8,9 @@ docker network create apt-network
 docker run -d --name apt-mongo --network apt-network --publish 27017:27017 --rm mongo
 
 # Importo i dati nella collection 'subscriptions':
-docker run --network apt-network -v "$PWD"/src/main/resources:/resources --rm mongo:latest mongoimport --host apt-mongo --db='subscriptions' --collection='orders' --headerline --file=resources/sample-orders.csv --type=csv
+docker run --network apt-network -v "$PWD"/src/main/resources:/resources \
+--rm mongo:latest mongoimport --host apt-mongo --db='subscriptions' \
+--collection='orders' --headerline --file=resources/sample-orders.csv --type=csv
 
 # Lancio la shell di MongoDB:
 # docker run -it --network apt-network --rm mongo:latest mongosh --host apt-mongo
